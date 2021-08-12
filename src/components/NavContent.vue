c3d4c10545ab67e44afa7287189e460c4c3ce064
Jonathan Martz <git@jmartz.de>
2021-08-12 20:56:54 +0200

    refactor navcontent

--- src/components/NavContent.vue
+++ src/components/NavContent.vue
@@ -2,20 +2,21 @@
   <div v-if="toggle" class="vc-nav-content">
     <div class="container">
       <div class="row">
-        <div v-for="(item, index) in items" :key="index" class="col-6 col-md-3">
+        <div v-for="(item, index) in items" :key="index" class="col-6 col-md-3 mt-2">
           <router-link :to="item.to" class="btn btn-primary d-block mb-2">{{
             item.title
           }}</router-link>
         </div>
-        <div class="col-6 col-md-3">
-          <router-link to="/checkout/cart/" class="btn btn-primary ml-2 btn-block">
-            <fontawesomeicon icon="shopping-cart" />
-          </router-link>
-        </div>
-        <div class="col-6 col-md-3">
-          <router-link to="/user/login/" class="btn btn-primary ml-2 btn-block">
-            <fontawesomeicon icon="user" />
-          </router-link>
+        <div class="col-6 col-md-3 mt-2">
+          <div class="btn-group">
+            <router-link to="/checkout/cart" class="btn btn-primary ml-md-2 d-block">
+              <fontawesomeicon icon="shopping-cart" />
+              {{ cart.length }}
+            </router-link>
+            <router-link to="/user/login/" class="btn btn-primary ml-md-2 d-block">
+              <fontawesomeicon icon="user" />
+            </router-link>
+          </div>
         </div>
       </div>
     </div>
@@ -35,6 +36,11 @@ export default {
     items: function () {
       return this.$store.state.nav.items;
     },
+    cart: function () {
+      return this.$store.state.cart;
+    },
   },
 };
 </script>
+
+<style scoped lang="less"></style>
