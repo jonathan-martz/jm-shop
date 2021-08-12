f2d9358d3311a2dbece83d7a1f82843e4412cb61
Jonathan Martz <git@jmartz.de>
2021-08-12 20:58:00 +0200

    add logic to cart store

--- src/store/cart.js
+++ src/store/cart.js
@@ -3,8 +3,45 @@ export default {
         items: []
     },
     mutations: {
-        'cart-add-item': (state, data) => {
-            state.items.push(data.item);
-        }      
+        "cart-load": (state, data) => {
+            state.items = JSON.parse(data.that.$localStorage.get('cart'));
+        },
+        "cart-clear": (state) => {
+            state.items = [];
+        },
+        "cart-update-qty": (state, data) => {
+            if (data.qty < 1) {
+                return false;
+            }
+            for (let i = 0; i < state.items.length; i++) {
+                if (data.id == state.items[i].id) {
+                    state.items[i].qty = data.qty;
+                }
+            }
+            data.that.$localStorage.set('cart', JSON.stringify(state.items));
+        },
+        "cart-add-item": (state, data) => {
+            let found = false;
+            if (state.items) {
+                for (let i = 0; i < state.items.length; i++) {
+                    if (data.item == state.items[i].product) {
+                        found = true;
+                        state.items[i].qty = parseInt(state.items[i].qty) + data.qty;
+                    }
+                }
+            }
+            else {
+                state.items = [];
+            }
+
+            if (!found) {
+                state.items.push({
+                    product: data.item,
+                    qty: data.qty,
+                    price: data.price
+                });
+            }
+            data.that.$localStorage.set('cart', JSON.stringify(state.items));
+        }
     }
 };
