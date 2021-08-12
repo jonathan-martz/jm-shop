89b75d70a15db2fc276fce58ad8ac6c9c9c060d8
Jonathan Martz <git@jmartz.de>
2021-08-12 20:58:39 +0200

    add new product view

---
+++ src/views/Product/View.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="vp-product-view">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-4 mt-2">
+          Image Slider Reviews
+          <img src="https://via.placeholder.com/400x480" class="img-responsive" />
+          <rating class="mt-2"></rating>
+        </div>
+        <div class="col-12 col-md-4">
+          <h5>Newest Reviews:</h5>
+          <div class="list-group">
+            <a
+              href="#"
+              class="list-group-item list-group-item-action active"
+              aria-current="true"
+            >
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small>3 days ago</small>
+              </div>
+              <p class="mb-1">Some placeholder content in a paragraph.</p>
+              <small>And some small print.</small>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small class="text-muted">3 days ago</small>
+              </div>
+              <p class="mb-1">Some placeholder content in a paragraph.</p>
+              <small class="text-muted">And some muted small print.</small>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small class="text-muted">3 days ago</small>
+              </div>
+              <p class="mb-1">Some placeholder content in a paragraph.</p>
+              <small class="text-muted">And some muted small print.</small>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small class="text-muted">3 days ago</small>
+              </div>
+              <p class="mb-1">Some placeholder content in a paragraph.</p>
+              <small class="text-muted">And some muted small print.</small>
+            </a>
+          </div>
+        </div>
+        <div class="col-12 col-md-4 mt-2">
+          <h1 class="h5">{{ item.name }}</h1>
+          <h6>SKU: {{ item.sku }}</h6>
+          <p class="short-desc">
+            {{ item.desc }}
+          </p>
+          <span class="price">9,99€</span>
+          <p class="stock">
+            Stock:
+            <span v-if="stock.qty > 0">Available</span>
+            <span v-else>not Available</span>
+          </p>
+          <div v-if="stock.qty > 0" class="qty">
+            <input
+              type="number"
+              v-model="qty"
+              placeholder="Menge"
+              value="1"
+              :max="stock.qty"
+            />
+          </div>
+          <div class="btn-group mt-2">
+            <button
+              @click="addToCart(item.id, qty, item.price)"
+              href=""
+              v-if="stock.qty > 0"
+              class="add-to-cart btn btn-primary"
+            >
+              Add to Cart
+            </button>
+            <a href="" class="add-to-wishlist btn btn-secondary">Add to Wishlist</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Rating from "@/components/Rating.vue";
+
+export default {
+  name: "ProductView",
+  data() {
+    return {
+      item: {},
+      stock: {},
+      qty: 1,
+    };
+  },
+  mounted() {
+    this.load();
+    this.$store.commit("cart-load", {
+      that: this,
+    });
+  },
+  methods: {
+    addToCart: function (item, qty, price) {
+      this.$store.commit("cart-add-item", {
+        that: this,
+        item: item,
+        qty: qty,
+        price: price,
+      });
+      this.$store.commit("cart-load", {
+        that: this,
+      });
+    },
+    load: function () {
+      if (this.$route.params.sku) {
+        fetch("https://ms.movie.jetzt/product/sku/" + this.$route.params.sku, {
+          method: "GET",
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.product) {
+              this.item = data.product;
+              this.loadStock(data.product.stock_id);
+            }
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      } else {
+        this.$router.push("/");
+      }
+    },
+    loadStock: function (id) {
+      fetch("https://ms.movie.jetzt/stock/" + id, {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.stock) {
+            this.stock = data.stock;
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    },
+  },
+  components: {
+    rating: Rating,
+  },
+};
+</script>
